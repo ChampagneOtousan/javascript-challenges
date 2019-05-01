91135709dae32ef3cddac163af249611598e65af
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-01 13:27:23 -0400

    added index main and js

---
+++ javascript-challenges/main.js
@@ -0,0 +1,8 @@
+for (let i = 99; i >= 1; i--) {
+  if (i != 1) { //if ((i-1) === 1)
+    console.log(i + ' crows on the wall. ' + i + ' crows. ' + ' 1 fell down and become a wight. ' + (i - 1) + ' crows on the wall. ')
+  }
+  else {
+    console.log(' 1 single crow. It fell down and became a wignt. There is no one left to defend Westeros now. ')
+  }
+}
