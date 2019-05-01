0baf823e207256b7c5b4e79e74d495b25e1fd3c1
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-01 13:46:51 -0400

    added first part of the assignment

--- javascript-challenges/main.js
+++ javascript-challenges/main.js
@@ -1,3 +1,13 @@
+// The first part
+var prices = [33, 26, 99, 120, 12, 45]
+var sum = 0
+
+for (let i = 0; i < prices.length; i++) {
+  sum += parseInt(prices[i])
+  // console.log("The sum is: ", sum);
+}
+
+// The second part
 for (let i = 99; i >= 1; i--) {
   if (i != 1) { //if ((i-1) === 1)
     console.log(i + ' crows on the wall. ' + i + ' crows. ' + ' 1 fell down and become a wight. ' + (i - 1) + ' crows on the wall. ')
