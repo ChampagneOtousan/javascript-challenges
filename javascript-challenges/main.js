d1c5cb86897416b8e679f9f850db7c4440a2d2a2
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-07 13:59:01 -0400

    added ugly ass pig latin

--- javascript-challenges/main.js
+++ javascript-challenges/main.js
@@ -2,10 +2,10 @@
 
 function reverseString(str) {
   return str.split("").reverse().join("")
-  // let splitString = str.split("")
-  // let revArray = splitString.reverse()
-  // let joinArray = revArray.join("")
-  // return joinArray
+  let splitString = str.split("")
+  let revArray = splitString.reverse()
+  let joinArray = revArray.join("")
+  return joinArray
 }
 
 // FizzBuzz Exercise
@@ -23,6 +23,19 @@ for (x=1; x <= 100; x++){
 }
 
 // Pig Latin Exercise
-function pigLatin(string) {
-  let word = string.split("").shift().reverse().add("ay").join("")
+
+function pigLatin(str) {
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  let wordStart = 0;
+  if (vowels.includes(str[0])) {
+    return str + "yay";
+  } else {
+    for (let char of str) {
+      if (vowels.includes(char)) {
+        wordStart = str.indexOf(char);
+        break;
+      }
+    }
+    return str.slice(wordStart) + str.slice(0, wordStart) + "ay";
+  }
 }
