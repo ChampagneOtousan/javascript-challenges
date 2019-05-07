1f88f5efd3c1ca73a37e15bb88820ddb1cf44eec
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-07 10:35:05 -0400

    FizzBuzz work

--- javascript-challenges/main.js
+++ javascript-challenges/main.js
@@ -1,18 +1,11 @@
-// The first part
-var prices = [33, 26, 99, 120, 12, 45]
-var sum = 0
-
-for (let i = 0; i < prices.length; i++) {
-  sum += parseInt(prices[i])
-  // console.log("The sum is: ", sum);
-}
-
-// The second part
-for (let i = 99; i >= 1; i--) {
-  if (i != 1) { //if ((i-1) === 1)
-    console.log(i + ' crows on the wall. ' + i + ' crows. ' + ' 1 fell down and become a wight. ' + (i - 1) + ' crows on the wall. ')
-  }
-  else {
-    console.log(' 1 single crow. It fell down and became a wignt. There is no one left to defend Westeros now. ')
-  }
+for (x=1; x <= 100; x++){
+    if( x % 3 == 0 ){
+        console.log("Fizz")
+    }
+    if( x % 5 == 0 ){
+        console.log("Buzz")
+    }
+    else if(( x % 3 != 0 ) && ( x % 5 != 0 )){
+        console.log(x)
+    }
 }
